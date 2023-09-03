a48a23f3a4e914ceeaad27f71a025f6d4a11cee2
Vincent Buchholz <vbu@miracle.dk>
2023-09-03 11:49:18 +0200

    refresh token

--- facades/productFacade.js
+++ facades/productFacade.js
@@ -5,30 +5,30 @@ function productFacade() {
 
     const URL = '/api/';
 
-    function getAllProducts() {
-        const opts = makeOptions('get',true);
-        return fetch(URL + 'products', opts).then((r) => r.json());
+    async function getAllProducts() {
+        const opts = makeOptions('get', true);
+        return fetch(URL + 'products', await opts).then((r) => r.json());
     }
 
     async function getProductById(id) {
         const opts = makeOptions('get', true);
-        return await fetch(URL + 'products/' + id, opts).then((r) => r.json());
+        return await fetch(URL + 'products/' + id, await opts).then((r) => r.json());
     }
 
     async function updateProduct(productObj) {
         const opts = makeOptions('put', true, productObj);
-        await fetch(URL + 'products/' + productObj.id, opts);
+        await fetch(URL + 'products/' + productObj.id, await opts);
 
     }
 
     async function deleteProductById(id) {
         const opts = makeOptions('delete', true);
-        await fetch(URL + 'products/' + id, opts);
+        await fetch(URL + 'products/' + id, await opts);
     }
 
     async function createProduct(productObj) {
         const opts = makeOptions('post', true, productObj);
-        await fetch(URL + 'products/', opts);
+        await fetch(URL + 'products/', await opts);
     }
 
 
